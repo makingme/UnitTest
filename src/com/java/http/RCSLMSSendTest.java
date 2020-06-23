89744818fa2284030fcce2720f7890d9735c3732
kibumkim <kibumkim@odinue.net>
2020-06-23 17:57:46 +0900

    update

---
+++ src/com/java/http/RCSLMSSendTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.java.http;
+
+/**
+
+  * @FileName : RCSLMSSendTest.java
+
+  * @Project : UnitTest
+
+  * @Date : 2020. 6. 23. 
+
+  * @작성자 : kibumkim
+
+  * @변경이력 :
+
+  * @프로그램 설명 :
+
+  */
+public class RCSLMSSendTest {
+
+}
